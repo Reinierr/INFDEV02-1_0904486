e62a601b0b0ac6b01ccaca33dd9789d9117da924
Reinierr <reinier.dorst1@hotmail.com>
2015-09-15 15:44:27 +0200

    commit

--- WebProject1/WebProject1/module1.py
+++ WebProject1/WebProject1/module1.py
@@ -1,2 +1,12 @@
 ﻿userName = input("Uw naam: ")
 print("Welkom " + userName)
+
+size = 10
+inner = size - 2
+staticinner = size - 2
+print ('*' * size)
+for i in range(inner):
+    newinner = inner = inner - 1
+    final = staticinner - newinner
+    print ('*' + newinner * '*' + final * ' ' + '*' )
+print ('*' * size)
